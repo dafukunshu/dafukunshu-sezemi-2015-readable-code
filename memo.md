0eed55ee5999da2f6943a4ee3a71d94906a06db9
fukuda <fukuda@Dummy-no-MacBook-Pro.local>
2015-06-06 12:00:47 +0900

    spec5&6: finished

--- memo.md
+++ memo.md
@@ -1,5 +1,7 @@
 ### 
 
-```c
-
+```python
+print_all_recipe(recipes)
+print_selected_recipe(recipes, id)
 ```
+allの方が全部のレシピ、selectedの方がid選択して出力できていいと思いました。
--- recipe.py
+++ recipe.py
@@ -2,17 +2,30 @@
 
 import sys
 
+def print_all_recipe(recipes):
+  recipe_num = len(recipes)
+  i = 0
+  while i < recipe_num:
+    print "%d: %s" % (i+1, recipes[i])
+    i += 1
+
+def print_selected_recipe(recipes, id):
+  print "%d: %s" % (recipe_id, recipes[recipe_id - 1])
+
 argv = sys.argv
-data_file = argv[1]
+argv_len = len(argv)
+if argv_len <= 2:
+  print "Usage: python %s recipe-data recipe_id"
+  quit()
+
+recipe_fname = argv[1]
+recipe_id = int(argv[2])
 
 # read data
-dataf = open(data_file, 'r')
-recipes = dataf.read().splitlines()
-recipe_num = len(recipes)
+recipef = open(recipe_fname, 'r')
+recipes = recipef.read().splitlines()
 
 # output data
-i = 0
-while i < recipe_num:
-  print recipes[i]
-  i += 1
-
+# print "%d: %s" % (recipe_id, recipes[recipe_id - 1])
+# print_all_recipe(recipes)
+print_selected_recipe(recipes, id)
